9b6685cc6fe07238b0bf18de472e29c06d94597a
Elto Borges <eltobrgs@192.168.1.19>
2024-11-08 23:57:30 -0300

    minha primeira pagina usando SpringBoot

---
+++ SpringBoot/firstpage/src/main/java/com/example/firstpage/FirstpageApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstpageApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/firstpage/src/test/java/com/example/firstpage/FirstpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
