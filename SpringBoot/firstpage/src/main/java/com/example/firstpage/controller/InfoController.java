1467cc30395b388c76f60aafe852f419550d694b
Elto Borges <eltobrgs@192.168.1.19>
2024-11-09 21:11:07 -0300

    adicionando atividade completa e caprichada de springboot

--- SpringBoot/firstpage/src/main/java/com/example/firstpage/controller/InfoController.java
+++ SpringBoot/firstpage/src/main/java/com/example/firstpage/controller/InfoController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class InfoController {
 
-    @GetMapping("/home")
-    public String home() {
-        return "home";  // Retorna o template "home.html"
+    @GetMapping("/info")
+    public String info() {
+        return "info";  
     }
 }
--- SpringBoot/firstpage/src/main/java/com/example/firstpage/controller/TutorialController.java
+++ SpringBoot/firstpage/src/main/java/com/example/firstpage/controller/TutorialController.java
@@ -4,10 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
-
-    @GetMapping("/home")
-    public String home() {
-        return "home";  // Retorna o template "home.html"
+public class TutorialController {
+    @GetMapping("/tutorial")
+    public String tutorial() {
+        return "tutorial";  
     }
 }
